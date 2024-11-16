8bc9d162a370c5a852f428a6657ad0e317ed3df5
Olle Modin <ollemodin@outlook.com>
2024-11-16 08:39:14 +0100

    Maybe not returning from the function'll work?

--- airootfs/etc/skel/rensetup/src/App.vue
+++ airootfs/etc/skel/rensetup/src/App.vue
@@ -32,22 +32,20 @@ async function check_internet() {
         alert(
             "An internet connection is required to install additional software. Please connect to the internet and try again!",
         );
-        return false;
     }
-    return true;
 }
 check_internet();
 // Install additional software
 document.getElementById("additional").innerHTML =
     "Please wait while non-essential improvements are being applied to RenOS...";
 async function install_other() {
-    while ((await check_internet()) != true) {
+    await check_internet().then(async () => {
         await invoke("install_other").then(async () => {
             await invoke("gdm_logo_fix");
             document.getElementById("additional").innerHTML =
                 "Select the additional software you want below:";
         });
-    }
+    });
 }
 install_other();
 let software = [stk, vscode, steam, spotify];
